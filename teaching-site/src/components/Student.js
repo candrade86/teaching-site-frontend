a8259148a9169e99393cc062af96b82c90798136
Carlos Andrade <carlos.andrade86@outlook.com>
2018-09-23 23:49:09 -0400

    created a temporary logout button on the student profile page

--- teaching-site/src/components/Student.js
+++ teaching-site/src/components/Student.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import requireAuth from '../hoc/requireAuth';
+import { signOut } from '../actions';
 
 class Student extends Component {
   render() {
     return (
       <div>
+        <h1 style={{fontSize: '3rem'}} onClick={()=> this.signOut()}>LOG OUT </h1>
         <h1>HOME PAGE</h1>
       </div>
     )
   }
 }
 
-export default requireAuth(Student)
+export default connect(null, { signOut })(requireAuth(Student));
