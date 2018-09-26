55c0b5086902fc176aba8a4b7920d99e0b5f7e6e
Carlos Andrade <carlos.andrade86@outlook.com>
2018-09-26 18:10:14 -0400

    changed what calendar I would be using

--- teaching-site/src/components/Scheduler.js
+++ teaching-site/src/components/Scheduler.js
@@ -1,73 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Calendar from 'react-big-calendar';
+import moment from 'moment-timezone';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 
-import FullCalendar from 'fullcalendar-reactwrapper';
-import 'fullcalendar-reactwrapper/dist/css/fullcalendar.min.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import {
+    Container
+} from '../styled-components/Scheduler';
+
+// Calendar.setLocalizer(Calendar.momentLocalizer(moment));
+const localizer = Calendar.momentLocalizer(moment)
+
+const DnDCalendar = withDragAndDrop(Calendar);
 
 class Scheduler extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+    state = {
         events:[
-                    {
-                        title: 'All Day Event',
-                        start: '2017-05-01'
-                    },
-                    {
-                        title: 'Long Event',
-                        start: '2017-05-07',
-                        end: '2017-05-10'
-                    },
-                    {
-                        id: 999,
-                        title: 'Repeating Event',
-                        start: '2017-05-09T16:00:00'
-                    },
-                    {
-                        id: 999,
-                        title: 'Repeating Event',
-                        start: '2017-05-16T16:00:00'
-                    },
-                    {
-                        title: 'Conference',
-                        start: '2017-05-11',
-                        end: '2017-05-13'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2017-05-12T10:30:00',
-                        end: '2017-05-12T12:30:00'
-                    },
-                    {
-                        title: 'Birthday Party',
-                        start: '2017-05-13T07:00:00'
-                    },
-                    {
-                        title: 'Click for Google',
-                        url: 'http://google.com/',
-                        start: '2017-05-28'
-                    }
-                ],		
-        }
-      }
+            {
+                start: new Date(),
+                end: new Date(moment().add(1, 'days')),
+                title: 'Carlos is amazing'
+            }
+        ]
+    };
+
+    onEventResize = (type, { event, start, end, allDay }) => {
+        this.setState(state => {
+            state.events[0].start = start;
+            state.events[0].end = end;
+            return { events: state.events };
+        });
+    };
+
+    onEventDrop = ({ event, start, end, allday}) => {
+        console.log(start);
+    };
 
   render() {
     return (
-      <div id="example-component">
-        <FullCalendar
-             id = "your-custom-ID"
-         header = {{
-            left: 'prev,next today myCustomButton',
-            center: 'title',
-            right: 'month,basicWeek,basicDay'
-        }}
-         defaultDate={'2017-09-12'}
-        navLinks= {true} // can click day/week names to navigate views
-        editable= {true}
-        eventLimit= {true} // allow "more" link when too many events
-        events = {this.state.events}	
-    />
-      </div>
-
+      <Container>
+          <DnDCalendar
+            localizer={localizer}
+            defaultDate={new Date()}
+            defaultView='month'
+            events={this.state.events}
+            onEventDrop={this.onEventDrop}
+            onEventResize={this.ondEventResize}
+            resizable
+            style={{ height: '100vh', background: 'white' }}
+          />
+      </Container>
     )
   }
 }
