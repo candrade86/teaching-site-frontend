f20da8aa942f8384c457d9c29973d710c6ededa9
Carlos Andrade <carlos.andrade86@outlook.com>
2018-09-29 16:35:00 -0400

    created 2 new action types for fetching events and fixed an error with the signup post route

--- teaching-site/src/actions/types.js
+++ teaching-site/src/actions/types.js
@@ -2,4 +2,7 @@ export const AUTH_USER = 'auth_user';
 export const SIGNING_UP = 'signing_up';
 export const SIGNING_IN = 'signing_in';
 
+export const FETCHING_EVENTS = 'getting_events';
+export const FETCHED_EVENTS = 'got_events';
+
 export const ERROR = 'error';
