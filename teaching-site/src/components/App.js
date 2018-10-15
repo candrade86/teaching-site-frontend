523adafe1c0d96a693f62bdf0e7ced429527caa6
Carlos Andrade <carlos.andrade86@outlook.com>
2018-10-15 19:34:32 -0400

    used react router pathing to display the navbar on all the pages except signin and signup

--- teaching-site/src/components/App.js
+++ teaching-site/src/components/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import Nav from './UI/Nav';
 import { Container } from '../styled-components/App';
-import Signup from './Signup';
 
 export default class App extends Component {
   
   render(){
     return (
       <Container>
-        <Nav/>
         { this.props.children }
-        
       </Container>
     );
   }
--- teaching-site/src/index.js
+++ teaching-site/src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Root from '../src/Root';
+import Nav from './components/UI/Nav';
 import App from './components/App';
 import Signup from './components/Signup';
 import Signin from './components/Signin';
 import Student from './components/Student';
 import Scheduler from './components/Scheduler';
 import Billing from './components/Billing';
+
 
 import registerServiceWorker from './registerServiceWorker';
 
@@ -19,6 +21,7 @@ ReactDOM.render(
         <BrowserRouter>
             <App>
                 <Route path='/' exact component={Signup} />
+                <Route path='/(student|scheduler|billing)/' component={Nav} />
                 <Route path='/signin' component={Signin} />
                 <Route path='/student/:id' component={Student} />
                 <Route path='/scheduler/:id' component={Scheduler} /> 
