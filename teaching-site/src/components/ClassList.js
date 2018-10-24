d7ec119ac170251f0210f2dd0fe90a6e05979e15
Carlos Andrade <carlos.andrade86@outlook.com>
2018-10-23 23:45:24 -0400

    mapped props passed into ClassList to display a simple list of all classes the student has had already and that are upcoming. Further date formatting and prettification required.

--- teaching-site/src/components/ClassList.js
+++ teaching-site/src/components/ClassList.js
@@ -2,9 +2,18 @@ import React from 'react'
 import { Fragment } from 'react';
 
 const ClassList = props => {
+    let classes = props.classes.map((c, i)=> {
+        return (
+            <div style={{ color: 'white', fontSize: '3rem' }}>
+                <h3>{c.start}</h3>
+            </div>
+        )
+    })
+
   return (
     <Fragment>
         <h1 style={{ color: 'white', fontSize: '3rem' }}> Class list will render here</h1>
+        {classes}    
     </Fragment>
   )
 }
