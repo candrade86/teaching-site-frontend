a0508486318c69bea68e4664bfdd648370a30c1e
Carlos Andrade <carlos.andrade86@outlook.com>
2018-10-24 23:58:02 -0400

    parsed the month and number day of each event and added it to the mapped list.

--- teaching-site/src/components/ClassList.js
+++ teaching-site/src/components/ClassList.js
@@ -3,9 +3,12 @@ import { Fragment } from 'react';
 
 const ClassList = props => {
     let classes = props.classes.map((c, i)=> {
+        let month = c.start.slice(4,7)
+        let nDate = c.start.slice(7,10)
         return (
             <div key={c._id} style={{ color: 'white', fontSize: '3rem' }}>
-                <h3>{c.day}{c.start}</h3>
+                {console.log('hey', month)}
+                <h3>{`${c.day} ${Math.abs(month)} ${Math.abs(nDate)}`}</h3>
             </div>
         )
     })
