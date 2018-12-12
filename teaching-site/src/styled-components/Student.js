424cb62ea8a0338327d58d2124184e160f151dd9
Carlos Andrade <carlos.andrade86@outlook.com>
2018-12-11 23:16:10 -0500

    added fadein animation to student page.

--- teaching-site/src/styled-components/Student.js
+++ teaching-site/src/styled-components/Student.js
@@ -9,6 +9,26 @@ export const Container = styled.div`
     min-height: 100vh;
 
     /* border: solid red; */
+        
+-webkit-animation: fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+animation: fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+            
+@-webkit-keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 
 `
 export const Top = styled.div`
