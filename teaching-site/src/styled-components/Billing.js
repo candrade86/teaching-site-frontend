ede0bea7731852b59f5196988c5eaf2bdbb4a304
Carlos Andrade <carlos.andrade86@outlook.com>
2018-12-12 22:47:37 -0500

    added fadein animation to billing page.

--- teaching-site/src/styled-components/Billing.js
+++ teaching-site/src/styled-components/Billing.js
@@ -7,6 +7,27 @@ export const Container = styled.div`
     align-items: center;
     width: 100%;
     min-height: 100vh;
+
+        
+    -webkit-animation: fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+    animation: fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+                
+    @-webkit-keyframes fade-in {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+    }
+    @keyframes fade-in {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+    }
 `
 export const Header = styled.div`
     display: flex;
